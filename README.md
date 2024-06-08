1e8ecfecc8c471cde098a2fc26f8b8c82268e21c
monickark <monicka.rk@gmail.com>
2024-06-08 11:17:07 +0530

    Added Article Summary

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-"# GenAI_ChatGPT_OpenAI" 
+# GEN_AI TEXT_BASED TUTORIALS WITH OPEN_AI
 
 ## Dependencies
 pip install python-dotenv
@@ -15,4 +15,20 @@ python chat_prompt.py
 Yes, other Azure AI services also support customer managed keys. The ability to manage and control your own encryption keys is a feature provided by 
 Azure Key Vault, which can be used in conjunction with various Azure AI services like Azure Cognitive Services, Azure Machine Learning, and Azure Bot Service.
 ```
+
+### ARTICLE_SUMMARY
+```console
+python article_summary.py
+ * Serving Flask app 'article_summary'
+ * Debug mode: on
+WARNING: This is a development server. Do not use it in a production deployment. Use a production WSGI server instead.
+ * Running on http://127.0.0.1:5000
+Press CTRL+C to quit
+ * Restarting with stat
+ * Debugger is active!
+ * Debugger PIN: 107-684-231
+127.0.0.1 - - [08/Jun/2024 11:12:27] "GET / HTTP/1.1" 200 -
+```
+Run this url > http://127.0.0.1:5000 in browser and provide input and get summarized output
+![RESULT](output/image.png)
 
---
+++ article_summary.py
@@ -0,0 +1,40 @@
+from dotenv import load_dotenv
+import os
+from openai import AzureOpenAI
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+load_dotenv()
+
+client = AzureOpenAI(
+  azure_endpoint = os.getenv('AZURE_ENDPOINT'),
+  api_key=os.getenv('API_KEY'),  
+  api_version=os.getenv('API_VERSION')
+)
+ 
+
+def summarize_article(article_text):
+    print("inside summarize article fn : ", article_text)
+    response = client.chat.completions.create(     
+        model="gpt-35-turbo",         
+       # prompt="Summarize the following article:\n\n{article_text}",
+         messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"Summarize the following article:\n\n{article_text}?"},
+    ]      
+    )
+    print("after summarize article ")
+    summary = response.choices[0].message.content
+    return summary
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        _article_text = request.form["article_text"]
+        print("_article_text : ", _article_text[0])
+        summary = summarize_article(_article_text)
+        return render_template("index.html", summary=summary, article_text=_article_text)
+    return render_template("index.html", summary="", article_text="")
+
+if __name__ == "__main__":
+    app.run(debug=True)
